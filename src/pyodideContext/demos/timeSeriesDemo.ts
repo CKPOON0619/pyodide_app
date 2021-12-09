12d8bcd2884c407121e036cd937c5733520ea27b
CKPOON0619 <ckpoon19890129@gmail.com>
2021-12-09 00:49:13 +0000

    update demo script for seasonal decompose

--- src/pyodideContext/demos/timeSeriesDemo.ts
+++ src/pyodideContext/demos/timeSeriesDemo.ts
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from statsmodels.tsa.stattools import adfuller, acf, pacf
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.seasonal import seasonal_decompose
 ###<Context variables>###
 from js import timeSeries
 ###<Script>###
@@ -39,8 +40,11 @@ def difference(df,diff):
 #pacf
 
 ##3. Fitting ARIMA
-model = ARIMA(df_log, order=(2,1,2))
-results = model.fit(disp=-1)
+# model = ARIMA(df_ts['value'], order=(2,1,2))
+# results = model.fit(disp=-1)
+df_ts.index=pd.date_range("1980-1-1", freq="M", periods=99) # Cast the index to date for seasonal decomposition to work.
+res=seasonal_decompose(df_ts['value'])
+print(res.trend,res.seasonal,res.resid)
 `;
 const packages = ["pandas", "numpy", "statsmodels"];
 
